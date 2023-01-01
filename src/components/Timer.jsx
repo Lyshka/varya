52cee808d7b51aa593b8cb26d68e58a9d48f9684
Lyshka <53864647+Lyshka@users.noreply.github.com>
2023-01-01 03:14:07 +0300

    fix
    fix mobile

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -44,7 +44,7 @@ const Timer = () => {
 
 	return (
 		<>
-			<div className="flex gap-8">
+			<div className="flex gap-8 flex-wrap">
 				<div className="circle time">
 					<div className="dots before:content-[''] before:absolute before:top-[-3px] before:w-6 before:h-6 before:bg-[#ffffff] before:rounded-[50%] before:shadow-[0_0_20px,0_0_60px]" style={{rotate: `${day * 0.986}deg`}}>
 
@@ -126,7 +126,7 @@ const Timer = () => {
 				</div>
 			</div>
 
-			<h2 className="text-6xl text-white text-center leading-3">
+			<h2 className="text-6xl text-white text-center leading-10">
 				До моего дня рождения
 			</h2>
 		</>
